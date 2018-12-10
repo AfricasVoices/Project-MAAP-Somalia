25fba8519dba24bd5168f838daf7c18637d80049
princelySid <sidney.ochieng@gmail.com>
2018-12-10 10:51:27 +0300

    change to scripts

--- create_coda_files/docker-run.sh
+++ create_coda_files/docker-run.sh
@@ -5,7 +5,7 @@ set -e
 IMAGE_NAME=maap-get-coda-files
 
 # Check that the correct number of arguments were provided.
-if [[ $# -ne 8 ]]; then
+if [ $# -ne 8 ]; then
     echo "Usage: sh docker-run.sh <user> <json-input-path> <flow-name> <variable-name> <json-output-path> <coda-output-path> <coding-scheme> <auto-cleaner>"
     exit
 fi
@@ -24,7 +24,7 @@ AUTO_CLEANER=$8
 docker build -t "$IMAGE_NAME" .
 
 # Create a container from the image that was just built.
-container="$(docker container create --env USER="$USER" --env FLOW_NAME="$FLOW_NAME" --env VARIABLE_NAME="$VARIABLE_NAME" --env "AUTO_CLEANER" "$IMAGE_NAME")"
+container="$(docker container create --env USER="$USER" --env FLOW_NAME="$FLOW_NAME" --env VARIABLE_NAME="$VARIABLE_NAME" --env AUTO_CLEANER="$AUTO_CLEANER" "$IMAGE_NAME")"
 
 function finish {
     # Tear down the container when done.
@@ -34,13 +34,14 @@ trap finish EXIT
 
 # Copy input data into the container
 docker cp "$INPUT_JSON" "$container:/data/input.json"
+docker cp "$CODING_SCHEME" "$container:/data/coding_scheme.json"
 
 # Run the container
 docker start -a -i "$container"
 
 # Copy the output data back out of the container
 mkdir -p "$(dirname "$OUTPUT_JSON")"
-docker cp "$container:/data/output-json.json" "$OUTPUT_CODA"
+docker cp "$container:/data/output_json.json" "$OUTPUT_CODA"
 
 mkdir -p "$(dirname "$OUTPUT_CODA")"
-docker cp "$container:/data/output-coda.json" "$OUTPUT_CODA"
+docker cp "$container:/data/output_coda.json" "$OUTPUT_CODA"
